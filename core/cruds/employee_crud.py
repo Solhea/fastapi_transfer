28cdeb1fed990d9b36c0d7aef005ce5699ac5987
Solhea <66067916+Solhea@users.noreply.github.com>
2022-11-11 17:04:07 +0300

    Fix: optional operation field on employee

--- core/cruds/employee_crud.py
+++ core/cruds/employee_crud.py
@@ -4,15 +4,22 @@ from core.schemas.employee import EmployeeCreate, EmployeeUpdate
 from core.models import Employee as EmployeeTable
 from core.utils.helpers import update_optional_fields
 
+
 def get_employee(db: Session, employee_id: int):
     return db.query(EmployeeTable).filter(EmployeeTable.id == employee_id).first()
+
 
 def get_all_employees(db: Session):
     return db.query(EmployeeTable).all()
 
+
 def create_employee(db: Session, employee: EmployeeCreate):
     db_employee = EmployeeTable(
-        first_name=employee.firstName, last_name=employee.lastName, email=employee.email, phone=employee.phone, address=employee.address,  department=employee.department, lat=employee.lat, lng=employee.lng, is_picked=employee.is_picked, operation_id=employee.operation_id)
+        first_name=employee.firstName, last_name=employee.lastName, email=employee.email, phone=employee.phone, address=employee.address,  department=employee.department, lat=employee.lat, lng=employee.lng, is_picked=employee.is_picked)
+
+    if employee.operation_id:
+        db_employee.operation_id = employee.operation_id
+
     db.add(db_employee)
     try:
         db.commit()
@@ -25,6 +32,7 @@ def create_employee(db: Session, employee: EmployeeCreate):
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST, detail="something went wrong")
     return db_employee
+
 
 def update_employee(db: Session, employee: EmployeeUpdate, employee_id: int):
     db_employee = get_employee(db, employee_id)
@@ -43,6 +51,7 @@ def update_employee(db: Session, employee: EmployeeUpdate, employee_id: int):
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST, detail="something went wrong")
     return db_employee
+
 
 def delete_employee(db: Session, employee_id: int):
     db_employee = get_employee(db, employee_id)
